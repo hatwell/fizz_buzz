fbb04c2196b4f60dae6ce0a849269847ef8b3aa6
Caroline Hatwell <caroline.hatwell@coopdigital.co.uk>
2021-09-27 18:51:51 +0100

    moved to number class

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,9 +1,19 @@
-def fizz_buzz(number):
-    if number % 3 == 0:
-        return 'Fizz'
-    if number % 5 == 0:
-        return 'Buzz'
-    if number % 7 == 0:
-        return 'Baz'
+class Number:
+    def __init__(self, value):
+        self.value = value
 
-    return number
+    def get_fizz_buzz_representation(self):
+        if self.value % 3 == 0:
+            return 'Fizz'
+        if self.value % 5 == 0:
+            return 'Buzz'
+        if self.value % 7 == 0:
+            return 'Baz'
+        return self.value
+
+
+
+def fizz_buzz(number):
+    number = Number(number)
+
+    return number.get_fizz_buzz_representation()
